1047e74cb338bdb87841d6baba2cd19c24ba4ba1
dell <piyush6348@gmail.com>
2017-02-11 00:58:48 +0530

    add: GSON model classes

---
+++ app/src/main/java/com/hackdtu/healthhistory/model/User.java
@@ -0,0 +1,44 @@
+package com.hackdtu.healthhistory.model;
+
+/**
+ * Created by dell on 2/11/2017.
+ */
+
+public class User {
+    private String first_name;
+    private String last_name;
+    private String phone;
+    private String blood_group;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+}
