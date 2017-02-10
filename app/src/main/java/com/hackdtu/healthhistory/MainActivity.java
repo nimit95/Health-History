ad4da5c85ad767965d7940aeac3986cf5f2adbe7
Nimit Agg <nimitagg95@gmail.com>
2017-02-11 02:21:59 +0530

    feat: Add MainActicity clicks

--- app/src/main/java/com/hackdtu/healthhistory/MainActivity.java
+++ app/src/main/java/com/hackdtu/healthhistory/MainActivity.java
@@ -1,13 +1,41 @@
 package com.hackdtu.healthhistory;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText adhaarNo, password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adhaarNo = (EditText) findViewById(R.id.aadhar);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new GetResponse().execute();
+            }
+        });
+    }
+
+    public class GetResponse extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... Void) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
