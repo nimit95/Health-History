60accb03497cc06e3dd365eb3b1d978cee396552
dell <piyush6348@gmail.com>
2017-06-21 13:49:27 +0530

    Upload Activity changes

--- app/src/main/java/com/hackdtu/healthhistory/activity/UploadActivity.java
+++ app/src/main/java/com/hackdtu/healthhistory/activity/UploadActivity.java
@@ -82,8 +82,7 @@ public class UploadActivity extends AppCompatActivity {
                     // Upload start
                     titleValue=title.getText().toString();
                     descriptionValue=description.getText().toString();
-                    //upload1(path);
-                    uploadMultipart(getApplicationContext());
+                    new demo().execute();
                 }
                 else
                 {
@@ -95,37 +94,7 @@ public class UploadActivity extends AppCompatActivity {
             }
         });
     }
-    public void uploadMultipart(final Context context) {
-        try {
-            String uploadId =
-                    new MultipartUploadRequest(context, Constants.UPLOAD_URL)
-                            // starting from 3.1+, you can also use content:// URI string instead of absolute file
-                            .addFileToUpload(path, "history_pic").addParameter("image_title","bjbj")
-                            .setNotificationConfig(new UploadNotificationConfig())
-                            .setBasicAuth("sahil","test12345")
-                            .setMaxRetries(2)
-                            .startUpload();
-        } catch (Exception exc) {
-            Log.e("AndroidUploadService", exc.getMessage(), exc);
-        }
-    }
-    public void upload1(String path) {
-        //if (!path.isEmpty()) {
-        //Bitmap bm = BitmapFactory.decodeFile(path);
-        Bitmap bm=getBitmap(path);
-        ByteArrayOutputStream bao = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.JPEG, 90, bao);
-        byte[] ba = bao.toByteArray();
-        ba1 = Base64.encodeToString(ba, Base64.DEFAULT);
 
-        Log.e("base64", "-----" + ba1);
-        // }
-        // Upload image to server
-
-        new demo().execute();
-
-
-    }
     private Bitmap getBitmap(String path) {
 
         ContentResolver mContentResolver=getContentResolver();
@@ -191,19 +160,19 @@ public class UploadActivity extends AppCompatActivity {
         }
     }
 
-    class demo extends AsyncTask<String, String, String> {
+    class demo extends AsyncTask<Void, Void, Void> {
         ProgressDialog pd;
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String boundary = "*****";
-        File sourceFile = new File(path);
-        URL url;
-        String lineEnd = "\r\n";
-        String serverResponseMessage;
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        String twoHyphens = "--";
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            pd.dismiss();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Bitmap image=getBitmap(path);
+            return null;
+        }
+
         @Override
         protected void onPreExecute() {
             pd = new ProgressDialog(UploadActivity.this);
@@ -211,76 +180,6 @@ public class UploadActivity extends AppCompatActivity {
             pd.setCancelable(false);
             pd.show();
             super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(String jsonArray) {
-            pd.dismiss();
-
-            Toast.makeText(getApplicationContext(),jsonArray,Toast.LENGTH_LONG).show();
-            super.onPostExecute(jsonArray);
-        }
-
-        @Override
-        protected String doInBackground(String... args) {
-
-          try {
-                Log.d("Picturepathnew",path);
-                FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                URL url = new URL(UPLOAD_URL);
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                conn.setRequestProperty("history_pic", path);
-                //conn.setRequestProperty("user_id", String.valueOf(2));
-                dos = new DataOutputStream(conn.getOutputStream());
-                dos.writeBytes(lineEnd);
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                bytesAvailable = fileInputStream.available();
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-
-                    dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                }
-                dos.writeBytes(lineEnd);
-                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                // Responses from the server (code and message)
-                serverResponseCode = conn.getResponseCode();
-                 serverResponseMessage = conn.getResponseMessage();
-                Log.d("JsonRES",String.valueOf(serverResponseCode));
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-          /*  HashMap<String, String> param = new HashMap<>();
-
-            param.put("image_title",titleValue);
-            //param.put("",descriptionValue);
-
-            Log.d("Imagestaring", String.valueOf(ba1));
-            json = jsonparsor.makeHttpRequest(UPLOAD_URL,"POST", param);
-            String res=json.opt("error_msg").toString();*/
-            Log.d("JsonArray", String.valueOf(json));
-            return "";
         }
     }
 }
